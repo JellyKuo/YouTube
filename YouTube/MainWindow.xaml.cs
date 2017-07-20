6bd5822a8681a56a0865e08549a672dc3988fb92
Jelly Kuo <jellykuo1234@gmail.com>
2017-07-20 23:04:57 +0800

    Created Toolbar

--- YouTube/MainWindow.xaml.cs
+++ YouTube/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace YouTube
         {
             InitializeComponent();
         }
+
+        private void AppExit_Click(object sender,EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
