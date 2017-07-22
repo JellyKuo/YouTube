d8e41a79f5c09204828bde3f5948bfb3707e3654
Jelly Kuo <jellykuo1234@gmail.com>
2017-07-22 11:59:26 +0800

    Finished browser

--- YouTube/App.xaml.cs
+++ YouTube/App.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CefSharp;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +17,19 @@ namespace YouTube
     /// </summary>
     public partial class App : Application
     {
+
+        public App()
+        {
+            var settings = new CefSettings()
+            {
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache), "CefSharp\\Cache"),
+                PersistSessionCookies = true,
+                Locale = System.Globalization.CultureInfo.CurrentCulture.Name,
+                PersistUserPreferences = true,
+                UserDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CefSharp\\UserData"),
+            };
+            settings.SetOffScreenRenderingBestPerformanceArgs();
+            Cef.Initialize(settings, true, null);
+        }
     }
 }
---
+++ YouTube/BrowserWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace YouTube
+{
+    /// <summary>
+    /// BrowserWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class BrowserWindow : Window
+    {
+        public BrowserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Browser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            Console.Write("Url: {0}",e.Url);
+            UpdateUrlBox(e.Url);
+        }
+
+        private void UpdateUrlBox(string url)
+        {
+            Action act = () => UrlBox.Text = url;
+            var dispatcher = UrlBox.Dispatcher;
+            if (dispatcher.CheckAccess())
+                act();
+            else
+                dispatcher.Invoke(act);
+        }
+
+        private void UrlBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+                return;
+            Browser.Load(UrlBox.Text);
+        }
+    }
+}
